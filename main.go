533669765e0bc67568d95c89f6ed0388f7a02654
th-release <cth.release@gmail.com>
2026-02-01 17:05:26 +0900

    feat: graceful shutdown

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"os/signal"
 	"remoter-boy-server/common"
 	"remoter-boy-server/grpc"
 	remoter "remoter-boy-server/proto_go"
+	"syscall"
+	"time"
 
 	grpcs "google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -25,7 +29,29 @@ func main() {
 	reflection.Register(srv)
 	log.Println("Remoter-Boy Start")
 
-	if err := srv.Serve(lis); err != nil {
-		panic(err)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.Serve(lis); err != nil {
+			panic(err)
+		}
+	}()
+
+	sig := <-quit
+	log.Printf("signal: %v", sig)
+
+	done := make(chan struct{})
+	go func() {
+		srv.GracefulStop()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		log.Println("Remoter-Boy Stopped")
+	case <-time.After(10 * time.Second):
+		log.Println("timeout! force stop")
+		srv.Stop()
 	}
 }
